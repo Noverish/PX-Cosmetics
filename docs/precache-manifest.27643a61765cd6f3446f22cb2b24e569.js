c72e1e1cb280024fbb767d23e2be033bcd05004d
Hyunsub Kim <embrapers263@gmail.com>
2020-02-15 15:42:11 +0900

    Added babel-plugin-import and set base url

---
+++ docs/precache-manifest.27643a61765cd6f3446f22cb2b24e569.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1832d9155b204bda421e547e6aa822a",
+    "url": "/PX-Cosmetics/index.html"
+  },
+  {
+    "revision": "336b5d5caa678c2f26a3",
+    "url": "/PX-Cosmetics/static/css/2.484485c1.chunk.css"
+  },
+  {
+    "revision": "b5e4330844c7b2d39e80",
+    "url": "/PX-Cosmetics/static/css/main.67b09f7a.chunk.css"
+  },
+  {
+    "revision": "336b5d5caa678c2f26a3",
+    "url": "/PX-Cosmetics/static/js/2.466eefc4.chunk.js"
+  },
+  {
+    "revision": "bfc08a62983de46a4daec9f8c3c657d8",
+    "url": "/PX-Cosmetics/static/js/2.466eefc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5e4330844c7b2d39e80",
+    "url": "/PX-Cosmetics/static/js/main.402d3c8d.chunk.js"
+  },
+  {
+    "revision": "6170f132f909e8bfabf1",
+    "url": "/PX-Cosmetics/static/js/runtime-main.47b2ac7f.js"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.85fff50572d6023eedf6fd2cbf645093.js"
+  "/PX-Cosmetics/precache-manifest.27643a61765cd6f3446f22cb2b24e569.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/PX-Cosmetics/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
