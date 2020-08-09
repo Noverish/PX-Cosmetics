273da1b91b6baa5463e9ea0608944b6e755c73bc
Hyunsub Kim <embrapers263@gmail.com>
2020-08-09 11:23:58 +0900

    Updated page

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/PX-Cosmetics/precache-manifest.c66df0c81cc296940a71776f9d6effca.js"
+  "/PX-Cosmetics/precache-manifest.3e56da22a21383d80de30e5253f26870.js"
 );
 
 self.addEventListener('message', (event) => {
