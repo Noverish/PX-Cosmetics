4cfd6dd0572fe8c6ae48d234ef7555018c3aae04
Hyunsub Kim <embrapers263@gmail.com>
2020-08-10 11:14:52 +0900

    Updates

---
+++ precache-manifest.65068088933df95d57ad5b02d98f30f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96007f45e6f1e3ff5d87848ee4c1f88c",
+    "url": "/PX-Cosmetics/index.html"
+  },
+  {
+    "revision": "e7b963bcbabddfd44dba",
+    "url": "/PX-Cosmetics/static/css/2.b0eeb5f9.chunk.css"
+  },
+  {
+    "revision": "14b6a763dac943f40830",
+    "url": "/PX-Cosmetics/static/css/main.189465b6.chunk.css"
+  },
+  {
+    "revision": "e7b963bcbabddfd44dba",
+    "url": "/PX-Cosmetics/static/js/2.d8a00cf9.chunk.js"
+  },
+  {
+    "revision": "9a0e6ad7df4effc3aa6ea7f039958117",
+    "url": "/PX-Cosmetics/static/js/2.d8a00cf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14b6a763dac943f40830",
+    "url": "/PX-Cosmetics/static/js/main.f009540e.chunk.js"
+  },
+  {
+    "revision": "f288b450767b60372c34",
+    "url": "/PX-Cosmetics/static/js/runtime-main.8b25851f.js"
+  }
+]);
